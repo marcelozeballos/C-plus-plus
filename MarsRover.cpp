1efa9a98ce2476507ba92fb86eab7d3580de0b4b
marcelozeballos <zeballosmarcelo@icloud.com>
2014-08-15 15:54:53 -0400

    Create MarsRover.cpp
    Mars Rover C++ Solution

---
+++ MarsRover.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int limX, limY; scanf("%d %d", &limX, &limY);
+    int x, y; char d;
+
+    while(scanf("%d %d %c", &x, &y,&d))
+    {
+        string commands; cin >> commands;
+        char pos[4] = {'N','E','S','W'};
+
+        int iter, length = commands.size();
+        for(int i =0;i < 4; i++){
+            if(pos[i] == d){
+                iter = i;
+                break;
+            }
+        }
+
+        for(int i =0; i < length; i++){
+            if(commands[i] == 'R')
+                iter++;
+            if(commands[i] == 'L')
+                iter--;
+
+            if(iter > 3)
+                iter = 0;
+            if(iter < 0)
+                iter = 3;
+
+            if(pos[iter] == 'N' && commands[i] == 'M')
+                y = y+1;
+            if(pos[iter] == 'S' && commands[i] == 'M')
+                y = y-1;
+            if(pos[iter] == 'E' && commands[i] == 'M')
+                x = x+1;
+            if(pos[iter] == 'W' && commands[i] == 'M')
+                x = x-1;
+        }
+        if(x < 0 || x > limX)
+            printf("ERROR\n");
+        else if(y <0 || y > limY)
+            printf("ERROR\n");
+        else
+        printf("%d %d %c \n", x, y, pos[iter]);
+
+    }
+    return 0;
+}
