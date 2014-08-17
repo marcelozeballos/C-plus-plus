c27536aff6e40564449a865040495cfad63702bb
marcelozeballos <zeballosmarcelo@icloud.com>
2014-08-16 22:03:59 -0400

    Solved

---
+++ Secuencia de Numeros I/main.cpp
@@ -0,0 +1,22 @@
+//serie de numeros 1,2,1,2,3,2,1 ....... 
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    int N, cnt = 0, i = 1, j = 1;
+    cin >> N;
+
+    while(N >= cnt)
+    {
+        for(i; cnt > i; i++)
+            cout << i << ",";
+
+        for(i; i > 1; i--)
+            cout << i << ",";
+
+        cnt++;
+    }
+    cout << j << endl;
+}
